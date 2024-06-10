39cf2a2e1f45d159bf5cb5577efb122003874f66
Leonardo <ldkprogramming@gmail.com>
2024-06-10 23:12:44 +0200

    truc

--- src/app/take-action/take-action.component.ts
+++ src/app/take-action/take-action.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-take-action',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './take-action.component.html',
   styleUrl: './take-action.component.css'
 })
